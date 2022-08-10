2412032854607e4ed7df9cc4e1fb6e9bba9e9b80
Shamimgardobuya <shamimobuya@gmail.com>
2022-08-10 13:13:51 +0300

    finished  subset,a subset a nested list of the main list containing possible lists.

--- revision.py
+++ revision.py
@@ -6,8 +6,12 @@
    #loop through each element in the first list
    #append the first and the second element
    #create another empty list,check if the items have been repeated in the new list
+   #ussing zip function loop through the list.
    #if not append to new list
    #create an empty list and append an element that has not been added in any list
+# import itertools-only available in another package.
+
+
 def subbing():
     original_list=[4,5,6]
     new_sub=[]
@@ -21,10 +25,25 @@ def subbing():
     third_sub=[]
     #use zip function to iterate over sec_sub and new_hub
     for (d,c) in zip(sec_sub,new_sub):
-        # if d in new_sub !=c in sec_sub:
-            print(d,c)
-            print('hello')
-            third_sub.append(d)
-    # print(third_sub)
+        if c!=d :  # will be true 
+           
+            third_sub.append(c)
+            # third_sub.append(c)
+    print(third_sub)   
+    four=[]
+    four.append(third_sub) 
+    #how can i get 5 and 6 only?
+    new2=[]
+    new2.append(original_list[0:])
+    five_sub=[5,6]
+    six_sub=[6,5]
+    seven_sub=[]
+    eight_sub=[]
+    seven_sub.append(five_sub[-1:])
+    eight_sub.append(six_sub[-1:])
+    print(four+sec_sub+new2+seven_sub,eight_sub)
+
+
+    # print(z)
 
 subbing()
